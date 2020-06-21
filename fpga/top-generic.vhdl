561385d65194a4abe52f02130bc9580d927495f0
Erik Müller <e-nospam@mueller-ramberg.de>
2020-06-21 17:28:36 +0200

    Add DRAM support interface for generic FPGAs. This is accomplished by exposing the Wishbone interface directly, albeit with a more compatible address bit mapping.

--- fpga/top-generic.vhdl
+++ fpga/top-generic.vhdl
@@ -11,6 +11,8 @@ entity toplevel is
 	RESET_LOW     : boolean  := true;
 	CLK_INPUT     : positive := 100000000;
 	CLK_FREQUENCY : positive := 100000000;
+	USE_DRAM      : boolean  := false;
+	DRAM_SIZE     : integer  := 0;
 	DISABLE_FLATTEN_CORE : boolean := false;
         UART_IS_16550 : boolean  := true
 	);
@@ -20,7 +22,20 @@ entity toplevel is
 
 	-- UART0 signals:
 	uart0_txd : out std_ulogic;
-	uart0_rxd : in  std_ulogic
+	uart0_rxd : in  std_ulogic;
+
+	-- DRAM main data wishbone connection
+	-- Remove the upper two address bits?
+	signal wb_dram_in_adr : out std_ulogic_vector(28 downto 0);
+	signal wb_dram_in_dat : out std_ulogic_vector(63 downto 0);
+	signal wb_dram_in_cyc : out std_ulogic;
+	signal wb_dram_in_stb : out std_ulogic;
+	signal wb_dram_in_sel : out std_ulogic_vector(7 downto 0);
+	signal wb_dram_in_we : out std_ulogic;
+	signal wb_dram_out_dat : in std_ulogic_vector(63 downto 0)
+		:= x"FFFFFFFFFFFFFFFF";
+	signal wb_dram_out_ack : in std_ulogic := '0';
+	signal wb_dram_out_stall : in std_ulogic := '0'
 	);
 end entity toplevel;
 
@@ -33,6 +48,10 @@ architecture behaviour of toplevel is
     -- Internal clock signals:
     signal system_clk : std_ulogic;
     signal system_clk_locked : std_ulogic;
+
+    -- DRAM main data wishbone connection
+    signal wb_dram_in       : wishbone_master_out;
+    signal wb_dram_out      : wishbone_slave_out;
 
 begin
 
@@ -68,6 +87,8 @@ begin
 	    RAM_INIT_FILE => RAM_INIT_FILE,
 	    SIM           => false,
 	    CLK_FREQ      => CLK_FREQUENCY,
+	    HAS_DRAM      => USE_DRAM,
+	    DRAM_SIZE     => DRAM_SIZE,
 	    DISABLE_FLATTEN_CORE => DISABLE_FLATTEN_CORE,
             UART0_IS_16550     => UART_IS_16550
 	    )
@@ -75,7 +96,27 @@ begin
 	    system_clk        => system_clk,
 	    rst               => soc_rst,
 	    uart0_txd         => uart0_txd,
-	    uart0_rxd         => uart0_rxd
+	    uart0_rxd         => uart0_rxd,
+	    wb_dram_in        => wb_dram_in,
+	    wb_dram_out       => wb_dram_out
 	    );
+
+
+    has_dram: if USE_DRAM generate
+    begin
+        -- Make the address compatible with "generic" RAM:
+        -- Cut off the segment prefix and narrow the address to word
+        -- granularity.
+        wb_dram_in_adr(28 downto 27) <= "00";
+        wb_dram_in_adr(26 downto 0) <= wb_dram_in.adr(29 downto 3);
+        wb_dram_in_dat <= wb_dram_in.dat;
+        wb_dram_in_cyc <= wb_dram_in.cyc;
+        wb_dram_in_stb <= wb_dram_in.stb;
+        wb_dram_in_sel <= wb_dram_in.sel;
+        wb_dram_in_we  <= wb_dram_in.we;
+        wb_dram_out.ack <= wb_dram_out_ack;
+        wb_dram_out.dat <= wb_dram_out_dat;
+        wb_dram_out.stall <= wb_dram_out_stall;
+    end generate;
 
 end architecture behaviour;
